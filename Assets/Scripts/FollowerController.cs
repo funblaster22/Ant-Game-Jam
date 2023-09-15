f3682b5e7c89adac17220d707daf345ea7c79015
Oliver Long <longoliver2005@gmail.com>
2023-09-15 19:17:50 -0400

    Movement and followers

--- Assets/Scripts/FollowerController.cs
+++ Assets/Scripts/FollowerController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class FollowerController : MonoBehaviour
 {
+    public float moveSpeed = 10;
+    GameObject player;
+    Rigidbody2D rb;
+    public Transform art;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = transform.GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        Vector3 moveDir = Vector3.Normalize(player.transform.position - transform.position);
+        rb.AddForce(moveDir*moveSpeed);
+        //taken from https://discussions.unity.com/t/lookat-2d-equivalent/88118
+        //trig to point followers at the player
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        art.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
     }
+    
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,12 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 10;
     public Camera cam;
     Rigidbody2D rb;
-    
+    public Transform art;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = transform.GetComponent<Rigidbody2D>();
-        
     }
 
     // Update is called once per frame
@@ -22,7 +22,13 @@ public class PlayerController : MonoBehaviour
         
         Vector3 moveDir = Vector3.Normalize(cam.ScreenToWorldPoint(Input.mousePosition) - transform.position);
         rb.velocity = moveDir*moveSpeed;
+        Debug.Log(moveDir*moveSpeed);
 
-        
+        //taken from https://discussions.unity.com/t/lookat-2d-equivalent/88118
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        art.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
     }
 }
