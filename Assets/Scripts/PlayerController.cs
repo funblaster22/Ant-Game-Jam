75f90026455b3ddb382568dc378cc88cd676df79
Oliver Long <longoliver2005@gmail.com>
2023-09-15 18:08:12 -0400

    more stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveForce = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody rb = transform.GetComponent(Rigidbody);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //
+        Vector3 moveDir = (transform.position - Camera.ScreenToWorldPoint(Input.mousePosition)).normalized;
+        rb.addforce(moveDir*moveForce)
         
+
     }
 }
