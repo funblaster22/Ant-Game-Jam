2975e2045b6bddcc16c7ff4b20fb61d9c5d4f672
Oliver Long <longoliver2005@gmail.com>
2023-09-16 12:00:00 -0400

    spinning pickups

--- Assets/Scripts/AntPickupController.cs
+++ Assets/Scripts/AntPickupController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class AntPickupController : MonoBehaviour
 {
     public GameObject antFollower;
+    [SerializeField] float spinSpeed = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,7 @@ public class AntPickupController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0,0,spinSpeed);
     }
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.gameObject.CompareTag("Player")){
