16ba0312bb390394f319c08fddcc620793327f46
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-09-16 11:50:12 -0400

    rename Goal

--- Assets/Scripts/FoodGoal.cs
+++ Assets/Scripts/FoodGoal.cs
@@ -18,7 +18,7 @@ public class Goal : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Food") {
+        if (collision.gameObject.CompareTag("Food")) {
             Destroy(collision.gameObject);
             GameState.Points++;
         }
