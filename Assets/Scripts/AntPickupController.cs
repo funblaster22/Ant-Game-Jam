4cdccf9b539c197e22d68b3e2ac48022f05f05b2
Oliver Long <longoliver2005@gmail.com>
2023-09-15 20:44:01 -0400

    made pickup

--- Assets/Scripts/AntPickupController.cs
+++ Assets/Scripts/AntPickupController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AntPickupController : MonoBehaviour
@@ -14,5 +15,13 @@ public class AntPickupController : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnTriggerEnter(Collider2D collision){
+        if(collision.transform.tag == "Player"){
+            GameState.followerCount += 1;
+            Debug.Log("Picked up an ant");
+            gameObject.SetActive(false);
+
+        }
     }
 }
