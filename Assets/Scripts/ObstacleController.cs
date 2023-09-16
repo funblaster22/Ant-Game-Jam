dfe25712677aeeda34ccce09ab05eb283b57d8dc
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-09-16 11:08:37 -0400

    fix food colection

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -19,7 +19,7 @@ public class ObstacleController : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (antsRequired >= GameState.FollowerCount) {
+        if (antsRequired <= GameState.FollowerCount) {
             Destroy(gameObject);
         }
     }
