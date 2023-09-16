003b8cbae5d68c1600cf2cc9ebb4c34c48304a75
saraxiao0 <xiao286@purdue.edu>
2023-09-16 00:27:04 -0700

    Fix GameState

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,23 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-    public int followerCount = 1;
+    public static int followerCount = 1;
 
-    public int Points { get; set; }
-    public int TimeLeft { get; set; }
+    public int Points { get; set; } = 0;
+    public float TimeLeft { get; set; } = 100;
+
+    public Boolean timerIsRunning { get; set; } = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        TimeLeft = 0;    
     }
 
     // Update is called once per frame
     void Update()
     {
-        Points++;    
+        Points++;
+
+        if (timerIsRunning)
+        {
+            if (TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                TimeLeft = 0;
+                timerIsRunning = false;
+            }
+        }
     }
 }
